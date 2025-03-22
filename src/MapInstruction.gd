00c0ce81bcbf52cb0e96f693fb92c5ec00f1d99d
Nicole Arruda <narruda789@gmail.com>
2025-03-22 17:19:04 -0400

    #2 Map shows for current area
    Some config stuff snuck in here, too...

--- src/MapInstruction.gd
+++ src/MapInstruction.gd
@@ -1,11 +1,19 @@
 class_name MapInstruction extends Instruction
 
-var _maps = null
+var _areas = null
 var _current_area = null
 
-func _init(maps, current_area):
-    _maps = maps
-    _current_area = current_area
+func _init(areas, current_area):
+	_areas = areas
+	_current_area = current_area
 
 func execute():
-    return ""
+	if "map" in _current_area:
+		var map_name = _current_area.map
+		var map_areas = _areas.maps[map_name].areas
+
+		var map = "%s:" % _areas.maps[map_name].name.to_upper()
+		for area in map_areas:
+			map += "\n  %s" % _areas[area].name
+		return map
+	return "???:\n (!) No map available!"
