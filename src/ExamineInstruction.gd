a563882dd760325d30f674bec7197aa77a0ee327
Nicole Arruda <narruda789@gmail.com>
2025-03-22 01:02:19 -0400

    Can take items from a POI

--- src/ExamineInstruction.gd
+++ src/ExamineInstruction.gd
@@ -1,13 +1,24 @@
 class_name ExamineInstruction extends Instruction
 
 var _area = null
-var _target = null
+var _poi = null
 
-func _init(area, target):
+func _init(area, poi):
 	_area = area
-	_target = target
+	_poi = poi
 
 func execute():
-	if _target not in _area.poi:
+
+	if _poi not in _area.poi:
 		return "Can't examine that."
-	return _area.poi[_target].description
+
+	var poi = _area.poi[_poi]
+	var message = ""
+	message += poi.description
+
+	if "items" in poi and not poi.items.is_empty():
+		message += "\n"
+		for item in poi.items:
+			message += "\n%s" % poi.items[item].beforePickup
+
+	return message
---
+++ tests/doubles/InstructionDouble.gd
@@ -0,0 +1,6 @@
+class_name InstructionDouble extends Instruction
+
+var was_execute_called = false
+
+func execute():
+    was_execute_called = true
