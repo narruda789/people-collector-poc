94c24888199cf1ecdd65c40d1c313ea630d217a9
Nicole Arruda <narruda789@gmail.com>
2025-03-23 01:56:41 -0400

    Complete downgrade to 4.2

--- tests/unit/MapInstructionTest.gd
+++ tests/unit/MapInstructionTest.gd
@@ -9,22 +9,61 @@ var _mock_areas = {
                 "stadium",
                 "frita batidos"
             ]
+        },
+        "lansing" : {
+            "name" : "Lansing",
+            "areas" : [
+                "frandor"
+            ]
         }
     },
     "library" : {
-        "name" : "Library"
+        "name" : "Library",
+        "map" : "ann arbor"
     },
     "stadium" : {
-        "name" : "Stadium"
+        "name" : "Stadium",
+        "map" : "ann arbor"
     },
     "frita batidos" : {
-        "name" : "Frita Batidos"
+        "name" : "Frita Batidos",
+        "map" : "ann arbor"
+    },
+    "frandor" : {
+        "name" : "Frandor",
+        "map" : "lansing"
+    },
+    "mystery spot" : {
+        "name" : "Mystery Spot"
     }
 }
 
-func test_map_shows_all_areas():
-    var instruction = MapInstruction.new(_mock_areas.maps, "library")
+func test_map_shows_map_name():
+    var instruction = MapInstruction.new(_mock_areas, _mock_areas["frita batidos"])
+    var map = instruction.execute()
+    assert_string_contains(map, "ANN ARBOR")
+
+func test_map_of_ann_arbor():
+    var instruction = MapInstruction.new(_mock_areas, _mock_areas["library"])
     var map = instruction.execute()
     assert_string_contains(map, "Library")
     assert_string_contains(map, "Stadium")
     assert_string_contains(map, "Frita Batidos")
+
+func test_map_of_lansing():
+    var instruction = MapInstruction.new(_mock_areas, _mock_areas["frandor"])
+    var map = instruction.execute()
+    assert_string_contains(map, "Frandor")
+
+func test_map_shows_current_location():
+    var instruction = MapInstruction.new(_mock_areas, _mock_areas["stadium"])
+    var map = instruction.execute()
+    assert_string_contains(map, "  Library")
+    assert_string_contains(map, "  Stadium (Alya is here!")
+    assert_string_contains(map, "  Frita Batidos")
+
+func test_no_map_available():
+    var instruction = MapInstruction.new(_mock_areas, _mock_areas["mystery spot"])
+    var map = instruction.execute()
+    assert_string_contains(map, "???:")
+    assert_string_contains(map, " (!) No map available!")
