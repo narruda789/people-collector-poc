68b1153cab35c47c99e5fc90bd02a4734ac6d388
Nicole Arruda <narruda789@gmail.com>
2025-03-25 14:10:06 -0400

    gh-37 Show game intro on start/restart

--- src/instructions/RestartInstruction.gd
+++ src/instructions/RestartInstruction.gd
@@ -5,4 +5,11 @@ var _game_data_path = "res://data/alya.json"
 func execute():
 	GameData.initialize_from_json(_game_data_path)
 	Inventory.clear()
-	return GameData.areas[GameData.current_area]["intro"]
+
+	var message = ""
+	message += "\n\n\n\n\n"
+	message += GameData.areas["game intro"]
+	message += "\n\n==========\n\n"
+	message += GameData.areas[GameData.current_area]["intro"]
+
+	return message
