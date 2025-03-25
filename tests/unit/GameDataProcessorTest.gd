e085039130fe96440b70d45566d206c0bf214600
Nicole Arruda <narruda789@gmail.com>
2025-03-24 20:13:47 -0400

    gh-33 Stop passing entire data to instructions

--- tests/unit/GameDataProcessorTest.gd
+++ tests/unit/GameDataProcessorTest.gd
@@ -3,7 +3,7 @@ extends GutTest
 var _processor = GameDataProcessor.new()
 
 func before_each():
-	_processor.current_area = GameData.get_data()["alya's room"]
+	_processor.current_area = GameData.game_data()["alya's room"]
 
 func test_examine_instruction_sets_current_poi():
 	# todo: contact GUT guy with funny test double error...
@@ -16,14 +16,14 @@ func test_examine_instruction_sets_current_poi():
 func test_restart_instruction_resets_game_data():
 	_processor.current_area = "some area"
 	_processor.current_poi = "some poi"
-	GameData.set_data("some game data")
+	GameData.set_game_data("some game data")
 	Inventory.add(Item.new("some item", "Some Item"))
 
 	_processor.process_action(InstructionSet.RESTART)
 	
-	assert_eq(_processor.current_area, GameData.get_data()["alya's room"])
+	assert_eq(_processor.current_area, GameData.game_data()["alya's room"])
 	assert_null(_processor.current_poi)
-	assert_not_null(GameData.get_data())
+	assert_not_null(GameData.game_data())
 	assert_eq(Inventory.get_as_list(), [])
 
 func test_instruction_calls_execute():
