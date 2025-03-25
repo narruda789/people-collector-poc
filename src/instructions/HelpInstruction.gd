ae55f307767c9f334e89c1bb17f1860d2a924648
Nicole Arruda <narruda789@gmail.com>
2025-03-25 01:15:08 -0400

    gh-3 Instruction parsing ignores "to"

--- src/instructions/HelpInstruction.gd
+++ src/instructions/HelpInstruction.gd
@@ -2,13 +2,13 @@ class_name HelpInstruction extends Instruction
 
 func execute():
     var helpText = "HELP:"
-    helpText += "\n  examine <target> | Get more information about a target"
-    helpText += "\n  take <item>      | Pick up an item"
-    helpText += "\n  [lb]i[rb]nventory      | See all the items Alya is carrying"
-    helpText += "\n  [lb]m[rb]ap            | View a map of the area"
-    helpText += "\n  [lb]g[rb]o <location>  | Go to a location on the map"
-    helpText += "\n                   |"
-    helpText += "\n  restart          | Restart game from the beginning"
-    helpText += "\n  help             | Open this help menu"
+    helpText += "\n  examine <target>     | Get more information about a target"
+    helpText += "\n  take <item>          | Pick up an item"
+    helpText += "\n  [lb]i[rb]nventory          | See all the items Alya is carrying"
+    helpText += "\n  [lb]m[rb]ap                | View a map of the area"
+    helpText += "\n  [lb]g[rb]o (to) <location> | Go to a location on the map"
+    helpText += "\n                       |"
+    helpText += "\n  restart              | Restart game from the beginning"
+    helpText += "\n  help                 | Open this help menu"
 
     return helpText
