b4d27e0467075dcc0f8ff61e701c27a9b1eaf8a7
Nicole Arruda <narruda789@gmail.com>
2025-03-26 02:50:15 -0400

    Move hint text into areas.json

--- src/instructions/RestartInstruction.gd
+++ src/instructions/RestartInstruction.gd
@@ -7,8 +7,6 @@ func _execute() -> String:
 	var message = ""
 	message += "\n\n\n\n\n"
 	message += GameData.areas["game intro"]
-	message += "\n\n..."
-	message += "\n\n (!) HINT: Type 'continue' to start the game!\n\n"
 
 	GameData.next_text.append(GameData.areas[GameData.current_area]["intro"])
 
